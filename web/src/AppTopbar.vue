f3f7bbe253f5a9f8830699636ed255536530d9f5
Tate McCormick <tatelax@gmail.com>
2022-05-12 21:28:00 -0700

    Update AppTopbar.vue

--- web/src/AppTopbar.vue
+++ web/src/AppTopbar.vue
@@ -1,16 +1,18 @@
 <template>
 	<div class="layout-topbar">
-		<router-link to="/" class="layout-topbar-logo">
-			<img alt="Logo" :src="topbarImage()" />
-			<span>SAKAI</span>
-		</router-link>
 		<button class="p-link layout-menu-button layout-topbar-button" @click="onMenuToggle">
 			<i class="pi pi-bars"></i>
 		</button>
+		
+		<router-link to="/" class="layout-topbar-logo">
+			<img alt="Logo" :src="topbarImage()" />
+			<span>Merlin</span>
+		</router-link>
 
-		<button class="p-link layout-topbar-menu-button layout-topbar-button"
-			v-styleclass="{ selector: '@next', enterClass: 'hidden', enterActiveClass: 'scalein', 
-			leaveToClass: 'hidden', leaveActiveClass: 'fadeout', hideOnOutsideClick: true}">
+		<button class="p-link layout-topbar-menu-button layout-topbar-button" v-styleclass="{
+			selector: '@next', enterClass: 'hidden', enterActiveClass: 'scalein',
+			leaveToClass: 'hidden', leaveActiveClass: 'fadeout', hideOnOutsideClick: true
+		}">
 			<i class="pi pi-ellipsis-v"></i>
 		</button>
 		<ul class="layout-topbar-menu hidden lg:flex origin-top">
@@ -38,17 +40,17 @@
 
 <script>
 export default {
-    methods: {
-        onMenuToggle(event) {
-            this.$emit('menu-toggle', event);
-        },
+	methods: {
+		onMenuToggle(event) {
+			this.$emit('menu-toggle', event);
+		},
 		onTopbarMenuToggle(event) {
-            this.$emit('topbar-menu-toggle', event);
-        },
+			this.$emit('topbar-menu-toggle', event);
+		},
 		topbarImage() {
 			return this.$appState.darkTheme ? './images/logo-white.svg' : 'images/logo-dark.svg';
 		}
-    },
+	},
 	computed: {
 		darkTheme() {
 			return this.$appState.darkTheme;
