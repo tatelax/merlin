a432fb435075b0d2ae8c7eb89d67907302a8f49d
Stew Heckenberg <stewart.heckenberg@twobulls.com>
2023-01-01 19:30:01 +0800

    setup vscode launch config for nuxt and added websocket sessions to app

--- web/app.vue
+++ web/app.vue
@@ -1,10 +1,29 @@
 <script setup lang="ts">
 const { x, y } = useMouse()
+
+const appId = '6J2FnJq8hQ8mEJ5Q1vdT'
+const userId = 79
+const sessions = ref<any[]>([])
+const sessionData = ref<any[]>([])
+
+const loadSession = ({ sessionID }: any) => {
+  const url = `ws://localhost:2414/?userID=79&appID=${appId}&sessionID=${sessionID}`
+  const { status, data, send } = useWebSocket(url, { protocols: ['SessionObserver'] })
+  watch(status, status => status === 'OPEN' && send('gimme state update'))
+  watch(data, async data => sessionData.value.push(JSON.parse(await data.text())))
+}
+
+const getSessions = () => {
+  const url = `ws://localhost:2414/ws?userID=${userId}&appID=${appId}`
+  const { status, data, send } = useWebSocket(url, { protocols: ['GetSessionsList'] })
+  watch(status, status => status === 'OPEN' && send('getSessions'))
+  watch(data, data => sessions.value = JSON.parse(data))
+}
+
+onMounted(() => getSessions())
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  <div>pos: {{x}}, {{y}}</div>
+  <button class="underline text-sky-500" v-for="s in sessions" @click="loadSession(s)">Session {{ s.sessionID }}</button>
+  <pre class="text-sm">{{ sessionData }}</pre>
 </template>
