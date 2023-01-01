cd4be1dea1d6e511dd12191f60d684f60256a6b6
Stew Heckenberg <stewart.heckenberg@twobulls.com>
2023-01-01 21:31:11 +0800

    added test vue component/page with vueuse and yaml

---
+++ web/src/components/Test.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { onMounted, ref, watch } from 'vue';
+import { useMouse, useWebSocket } from '@vueuse/core'
+import YAML from 'yaml'
+
+const { x, y } = useMouse()
+
+const appId = '6J2FnJq8hQ8mEJ5Q1vdT'
+const userId = 79
+const sessions = ref<any[]>([])
+const sessionData = ref<any[]>([])
+
+const loadSession = ({ sessionID }: any) => {
+  const url = `ws://localhost:2414/?userID=79&appID=${appId}&sessionID=${sessionID}`
+  const { status, data, send } = useWebSocket(url, { protocols: ['SessionObserver'] })
+  watch(status, status => status === 'OPEN' && send('gimme state update'))
+  watch(data, async data => sessionData.value.push(JSON.parse(await data.text())))
+}
+
+const getSessions = () => {
+  const url = `ws://localhost:2414/ws?userID=${userId}&appID=${appId}`
+  const { status, data, send } = useWebSocket(url, { protocols: ['GetSessionsList'] })
+  watch(status, status => status === 'OPEN' && send('getSessions'))
+  watch(data, data => sessions.value = JSON.parse(data))
+}
+
+onMounted(() => getSessions())
+</script>
+
+<template>
+  <button class="underline text-sky-500" v-for="s in sessions" @click="loadSession(s)">Session {{ s.sessionID }}</button>
+  <pre class="text-sm" v-if="sessionData.length" v-text="YAML.stringify(sessionData)" />
+</template>
