2ab7bc3495c8e736a4e063fe8e0688dcafbc3468
Stew Heckenberg <stewart.heckenberg@twobulls.com>
2023-01-01 18:42:57 +0800

    added vueuse

--- web/app.vue
+++ web/app.vue
@@ -1,5 +1,10 @@
+<script setup lang="ts">
+const { x, y } = useMouse()
+</script>
+
 <template>
   <h1 class="text-3xl font-bold underline">
     Hello world!
   </h1>
+  <div>pos: {{x}}, {{y}}</div>
 </template>
